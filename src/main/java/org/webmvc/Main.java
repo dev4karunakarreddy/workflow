2b294282534b3f969f9ecda54e915e0a9bf5b887
karunakarreddy <dev4karunakarreddy@gmail.com>
2025-01-08 10:20:46 +0545

    initial commit

--- src/main/java/org/webmvc/Main.java
+++ src/main/java/org/webmvc/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             System.out.println("i = " + i);
         }
     }
